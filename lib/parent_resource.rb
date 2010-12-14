a8ec0035050ec0c8d1f0d77fda4e77c48d0b7a81
Yamada Masaki <masaki@hisme.net>
2010-12-14 14:01:06 +0900

    move module

--- lib/parent_resource.rb
+++ lib/parent_resource.rb
@@ -1,30 +1,8 @@
 # ParentResource
 module ParentResource
-  module ActMethods
-    def parent_resource(*keys)
-      keys.each do |key|
-        create_parent_resource(key)
-      end
-    end
-    
-    private
-    def create_parent_resource(key)
-      method_name = "_parent_#{key}".to_s
-      model = key.to_s.classify.constantize
-      key_name = "#{key}_id"
-      val = "@#{key}".to_sym
-      
-      define_method(method_name) do
-        if params[key_name]
-          instance_variable_set(val, model.find(params[key_name]))
-        end
-      end
-      private method_name
-      before_filter method_name
-    end
-  end
+  autoload :ControllerMethods, 'parent_resource/controller_methods'
 end
 
 class ActionController::Base
-  extend ParentResource::ActMethods
+  extend ParentResource::ControllerMethods
 end
---
+++ lib/parent_resource/controller_methods.rb
@@ -0,0 +1,24 @@
+module ParentResource::ControllerMethods
+  def parent_resources(*keys)
+    keys.each do |key|
+      create_parent_resource(key)
+    end
+  end
+  alias_method :parent_resource, :parent_resources
+  
+  private
+  def create_parent_resource(key)
+    method_name = "_parent_#{key}".to_s
+      model = key.to_s.classify.constantize
+    key_name = "#{key}_id"
+    val = "@#{key}".to_sym
+    
+    define_method(method_name) do
+      if params[key_name]
+        instance_variable_set(val, model.find(params[key_name]))
+      end
+    end
+    private method_name
+    before_filter method_name
+  end
+end
