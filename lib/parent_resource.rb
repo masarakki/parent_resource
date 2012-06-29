e4c56d88792938d30367292bb2df7a25b819cbd6
masarakki <masaki@hisme.net>
2012-06-29 09:44:43 +0900

    add spec

--- lib/parent_resource.rb
+++ lib/parent_resource.rb
@@ -1,8 +1,31 @@
-# ParentResource
+require 'active_support/concern'
+
 module ParentResource
-  autoload :ControllerMethods, 'parent_resource/controller_methods'
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def parent_resources(*keys)
+      keys.each do |key|
+        create_parent_resource(key)
+      end
+    end
+
+    private
+    def create_parent_resource(key)
+      model = key.to_s.classify.constantize
+      key_name = "#{key}_id".to_sym
+      val = "@#{key}".to_sym
+
+      define_method(key) do
+        unless instance_variable_defined?(val)
+          _params = params.symbolize_keys
+          res = _params.has_key?(key_name) ? model.find(_params[key_name]) : nil
+          instance_variable_set(val, res)
+        end
+        instance_variable_get(val)
+      end
+    end
+  end
 end
 
-class ActionController::Base
-  extend ParentResource::ControllerMethods
-end
+ActionController::Base.send(:include, ParentResource)
--- spec/parent_resource_spec.rb
+++ spec/parent_resource_spec.rb
@@ -1,5 +1,39 @@
 require 'spec_helper'
+require 'active_record'
+
+class User ; end
+class Book ; end
 
 describe ParentResource do
+  subject { @instance }
+  before do
+    @klass = Class.new do
+      include ParentResource
+    end
+  end
+  describe :class_methods do
+    subject { @klass }
+    it "create_parent_resource should received" do
+      @klass.should_receive(:create_parent_resource).with(:user)
+      @klass.should_receive(:create_parent_resource).with(:book)
 
+      @klass.parent_resources :user, :book
+    end
+  end
+  describe :instance_methods do
+    let(:user) { User.new }
+    before do
+      @klass.parent_resources :user, :book
+      @instance = @klass.new
+      @instance.stub(:params).and_return(:user_id => "1")
+    end
+    it "create user instance" do
+      User.should_receive(:find).with("1").and_return(user)
+      @instance.user.should == user
+    end
+    it "not create book instance" do
+      Book.should_not_receive(:find)
+      @instance.book.should be_nil
+    end
+  end
 end
