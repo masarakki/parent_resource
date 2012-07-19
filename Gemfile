cb3ee9c43f88dfe344e54589a261197b2e497f09
masarakki <masaki@hisme.net>
2012-07-20 03:17:37 +0900

    setup guard

--- Gemfile
+++ Gemfile
@@ -2,10 +2,13 @@ source :rubygems
 
 gem "rails"
 
-group :development do
+group :development, :test do
   gem "rspec"
   gem "bundler"
   gem "jeweler"
   gem 'guard'
+  gem 'spork'
+  gem 'guard-bundler'
+  gem 'guard-spork'
   gem 'guard-rspec'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,21 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'rspec'
-require 'action_controller'
-require 'parent_resource'
+require 'rubygems'
+require 'spork'
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+Spork.prefork do
+  $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+  $LOAD_PATH.unshift(File.dirname(__FILE__))
+  require 'rspec'
+  require 'action_controller'
+  require 'parent_resource'
 
-RSpec.configure do |config|
-  
+  Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+  RSpec.configure do |config|
+
+  end
 end
+
+Spork.each_run do
+end
+
+
